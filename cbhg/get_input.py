334ce2196dcafdb3b8060f0ccd6b3e7af97f59e9
王裴 <m17375069764@163.com>
2020-08-02 22:08:19 +0800

    初版本

--- cbhg/get_input.py
+++ cbhg/get_input.py
@@ -6,6 +6,29 @@ import numpy as np
 import tensorflow as tf
 from tacotron.models import modules
 
+
+
+def prenet_yhhc(input):
+	out = tf.nn.relu(tf.layers.dense(input, 256))
+	out = tf.layers.dense(out, 256)
+	return out
+
+def prenet2_yhhc(input):
+	out = tf.nn.relu(tf.layers.dense(input, 256))
+	out = tf.layers.dense(out, 80)
+	return out
+
+def cbhg_yhhc(input_lab, input_dim=None):
+	input_lab = prenet_yhhc(input_lab)
+	return modules.cbhg(inputs_lab,
+	                    input_dim,
+	                    True,
+	                    scope='cbhg_yhhc',
+	                    K=8,
+	                    projections=[256, input_dim],
+	                    depth=256)
+
+
 _pad = '_'
 
 phone_set =[_pad] + [ '~', ';', '；', '、', ':', 'pau', '：', 'sp', '，', ',', 'sil', '？', '！', '。', '!', '?', 'n', 's', 'ER', 'iong', 'AE',
@@ -97,7 +120,7 @@ for file in dir_lab[:1]:
 	inputs_mel.append(array_mel)
 	# print('mel_len',mel_len)
 inputs_lab = np.array(inputs_lab)
-# inputs_lab[:,:,4:] = inputs_lab[:,:,4:].astype('float32')
+# inputs_lab[:,:,4:] = inputs_lab[:,:,4:].astype  ('float32')
 inputs_mel = np.array(inputs_mel)
 
 input_phone_set_ids = tf.compat.v1.placeholder(dtype=tf.int32, shape=[None, None])
@@ -145,3 +168,49 @@ print(sess.run(tf.concat([a,b,c,d,e],axis=-1)))
 # print("test", inputs_lab[0])
 # print("mel", inputs_mel[0])
 
+batch_size = 250
+LEARNING_RATE_BASE = 0.01
+LEARNING_RATE_DECAY = 0.99
+n_batch = len(inputs_lab) // batch_size
+
+
+input_x = tf.compat.v1.placeholder(tf.float32, [None, 578])
+mel_y = tf.compat.v1.placeholder(tf.float32, [None, 80])
+cbhg_out = cbhg_yhhc(input_x)
+cbhg_out = prenet2_yhhc(cbhg_out)
+
+loss_batch = tf.reduce_mean(tf.square(cbhg_out, mel_y))
+global_step = tf.Variable(0, trainable=False)
+
+
+learning_rate = tf.compat.v1.train.exponential_decay(LEARNING_RATE_BASE, global_step,
+                                                     n_batch, LEARNING_RATE_DECAY)
+train = tf.compat.v1.train.AdamOptimizer(learning_rate).minimize(loss_batch, global_step=global_step)
+# out_y = cbhg_yhhc()
+saver = tf.train.Saver()
+import time
+
+
+with tf.compat.v1.Session() as sess:
+	start_time = time.perf_counter()
+	sess.run(tf.compat.v1.global_variables_initializer())
+	for epoch in range(100):
+		for batch in range(n_batch):
+			# for i in range(batch_size):  # load batch
+			# rnd_indices = np.random.randint(0, len(X_train))
+			# x_b = X_train[rnd_indices]
+			# y_b = Y_train[rnd_indices]
+			# if i < 1:
+			# 	batch_xs = [x_b]
+			# 	batch_ys = [y_b]
+			# else:
+			# 	batch_xs = np.append(batch_xs, [x_b], axis=0)
+			# 	batch_ys = np.append(batch_ys, [y_b], axis=0)
+			sess.run(train, feed_dict={input_x: inputs_lab[batch*batch_size:(batch+1)*batch_size, :],
+			                           mel_y: inputs_mel[batch*batch_size:(batch+1)*batch_size, :]})
+		if epoch % 4 == 0:
+			# acc = sess.run(accuracy, feed_dict={x: X_test, y: Y_test})
+			print(sess.run(loss_batch, feed_dict={input_x: inputs_lab[batch*batch_size:(batch+1)*batch_size, :],
+			                           mel_y: inputs_mel[batch*batch_size:(batch+1)*batch_size, :]}) )
+			saver.save(sess, "model.ckpt")
+	end_time = time.perf_counter()
